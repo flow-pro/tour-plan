f00496f96669c53eb2faa0b2020c2be6ff962b3c
flow-pro <vivalasdancers@gmail.com>
2020-08-04 22:02:47 +0300

    added: validation form

--- js/main.js
+++ js/main.js
@@ -45,4 +45,24 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "The name must be at least two letters",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please enter your phone number",
+        },
+      },
+    });
+  });
 });
